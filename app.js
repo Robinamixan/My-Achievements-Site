90e3fec8a00df042e9e3fa11690a7c12a7e97a7d
fgorodkovets <f.gorodkovets@itransition.com>
2023-04-28 14:22:36 +0200

    MA-15. Cleared code

--- app.js
+++ app.js
@@ -6,13 +6,15 @@ const appServer = require('./app-server');
 const INTERNAL_PORT = process.env.NODEJS_PORT;
 const MONGODB_URI = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`;
 
-const app = appServer.init();
+(() => {
+    const app = appServer.init();
 
-mongoose.set('strictQuery', false);
-mongoose.connect(MONGODB_URI)
-    .then(() => app.listen(INTERNAL_PORT))
-    .catch(error => {
-        console.log(error);
+    mongoose.set('strictQuery', false);
+    mongoose.connect(MONGODB_URI)
+        .then(() => app.listen(INTERNAL_PORT))
+        .catch(error => {
+            console.log(error);
 
-        return error;
-    });
+            return error;
+        });
+})();
